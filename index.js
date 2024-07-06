53acc0541e4fd161bc0c238adc9a0c16fee40df3
Mellinda wambui <mellindawambui@gmail.com>
2024-07-06 17:38:51 +0300

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+
+const drivers = ['Bobby','Sammy','Sally', 'Annette', 'Sarah','bobby']
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+
+
+
+
+
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+
+
+
+
+
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string);
+}
+
+
+
+
+
+
